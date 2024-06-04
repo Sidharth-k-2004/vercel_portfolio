7e2969300594e42493651b7139f487e652cb1031
sidharth k <sidharthkdinesan123@gmail.com>
2024-06-04 09:43:43 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -52,5 +52,6 @@ app.post('/connect', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  // console.log(`Server running on port ${port}`);
+  <div>server running</div>
 });
